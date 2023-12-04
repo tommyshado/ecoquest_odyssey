91b1d93438cc948e4f8f0a7cc0ba9fab24d64ac6
Bjorn Worrall <85297118+lasity34@users.noreply.github.com>
2023-12-04 19:27:30 +0200

    users

---
+++ .history/index_20231204192635.js
@@ -0,0 +1,26 @@
+import express from "express";
+import { engine } from 'express-handlebars'
+import dotenv from "dotenv";
+import pgPromise from "pg-promise";
+import session from 'express-session';
+import { createClient } from '@supabase/supabase-js'
+
+const app = express();
+
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
+
+app.engine("handlebars", engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+
+dotenv.config();
+
+
+const PORT = process.env.PORT || 3014;
+
+app.listen(PORT, function () {
+  console.log("App has started", PORT);
+});
